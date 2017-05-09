a407d30a6204430a8b832b73e4500730826704cb
luantinypulse <luan@tinypulse.com>
2017-05-10 03:30:21 +0700

    A pert of 2nd video Commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
 
   def self.search(search)
     where("title || body ILIKE ?", "%#{search}%")
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ApplicationRecord
-  has_many :comment, dependent: :destroy
-
+    belongs_to :article
 
   def self.search(search)
     where("title || body ILIKE ?", "%#{search}%")
