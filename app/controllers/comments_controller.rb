25adef823639d615d0cab5329c4585ad6aea90f7
luantinypulse <luan@tinypulse.com>
2017-05-10 12:25:10 +0700

    Half wayof 2nd video Commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :find_article
+  before_action :find_comment, only: [:edit, :update, :destroy]
 
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
 
     if @comment.save
@@ -11,25 +12,32 @@ class CommentsController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-  end
-
   def update
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
+    @comment.content = comment_params
     if @comment.update(comment_params)
-      redirect_to articles_path(@article)
+      redirect_to article_path(@article)
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def find_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def find_comment
+    @comment = @article.comments.find(params[:id])
   end
 
 end
